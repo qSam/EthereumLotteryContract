445dc1b460d1afc0711f5b547f1b3f3a23eec557
Qasim Laghari <go.jets.laghari@gmail.com>
2018-08-25 14:20:09 -0500

    Update compile.js and deploy.js to reflect Lottery Contract

--- compile.js
+++ compile.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const solc = require('solc');
 
 //Set path to fetch solidity file that needs to be compiled
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
-const source = fs.readFileSync(inboxPath, 'utf8');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 //Call the solidity compiler
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
